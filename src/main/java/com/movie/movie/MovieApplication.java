89394824dc9a1a1258cd98a43fe49df59b397708
Anshu Kumar <108130496+anshukumar11062@users.noreply.github.com>
2026-04-21 17:06:02 +0530

    test code

--- src/main/java/com/movie/movie/MovieApplication.java
+++ src/main/java/com/movie/movie/MovieApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 /**
  * | Author - Anshu Kumar
  * | Created On - 17/02/2024
- * | description - Main file of the Project Test Code
+ * | description - Main file of the Project
  * | Status - Closed
  */
 
