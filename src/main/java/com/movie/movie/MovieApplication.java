9c5aaecf4a6fc628f21c0197df70f70123c14ee4
Anshu Kumar <108130496+anshukumar11062@users.noreply.github.com>
2026-04-22 13:54:43 +0530

    Build Check

--- src/main/java/com/movie/movie/MovieApplication.java
+++ src/main/java/com/movie/movie/MovieApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 /**
  * | Author - Anshu Kumar
- * | Created On - 17/02/2024 Build Check
+ * | Created On - 17/02/2024 
  * | description - Main file of the Project
  * | Status - Closed
  */
